8c939abb7abaf7b1b38ed2aa2332bef8a98573cd
Clivern <hello@clivern.com>
2021-07-07 00:28:23 +0200

    update

--- src/db/client.rs
+++ src/db/client.rs
@@ -1,3 +1,18 @@
 // Copyright 2022 Clivern. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
+
+use crate::util;
+use postgres::{Client, NoTls};
+
+pub fn get_client() -> Result<Client, String> {
+    let rocket_config =
+        util::config::get_env("ROCKET_CONFIG", util::config::get_config_path().as_str());
+
+    let config = util::config::get_configs(rocket_config.to_string());
+
+    match Client::connect(config.global.db.as_str(), NoTls) {
+        Ok(client) => Ok(client),
+        Err(_err) => Err(String::from("Unable to establish database connection!")),
+    }
+}
