665703bea12b460ac7074204664f9c0b5d3aa125
Clivern <hello@clivern.com>
2021-07-07 00:31:05 +0200

    update

--- src/db/db.rs
+++ src/db/db.rs
@@ -17,35 +17,35 @@ pub fn tables() {
 
     match client.expect("failed to migrate").batch_execute(
         "
-		CREATE  TABLE IF NOT EXISTS ho_project (
-			id                   integer  NOT NULL  ,
-			name                 varchar(100)  NOT NULL  ,
-			vers                 varchar(100)  NOT NULL  ,
-			created_at           timestamp  NOT NULL  ,
-			updated_at           timestamp  NOT NULL  ,
-			username             varchar(100)  NOT NULL  ,
-			secret               varchar(100)  NOT NULL  ,
-			CONSTRAINT pk_ho_project PRIMARY KEY ( id )
-		 );
+        CREATE  TABLE IF NOT EXISTS ho_project (
+            id                   integer  NOT NULL  ,
+            name                 varchar(100)  NOT NULL  ,
+            vers                 varchar(100)  NOT NULL  ,
+            created_at           timestamp  NOT NULL  ,
+            updated_at           timestamp  NOT NULL  ,
+            username             varchar(100)  NOT NULL  ,
+            secret               varchar(100)  NOT NULL  ,
+            CONSTRAINT pk_ho_project PRIMARY KEY ( id )
+         );
 
-		CREATE  TABLE IF NOT EXISTS ho_state (
-			id                   integer  NOT NULL  ,
-			name                 varchar(100)    ,
-			val               text  NOT NULL  ,
-			created_at           timestamp    ,
-			updated_at           timestamp    ,
-			project_id           integer  NOT NULL  ,
-			CONSTRAINT state_pkey PRIMARY KEY ( id )
-		 );
+        CREATE  TABLE IF NOT EXISTS ho_state (
+            id                   integer  NOT NULL  ,
+            name                 varchar(100)    ,
+            val               text  NOT NULL  ,
+            created_at           timestamp    ,
+            updated_at           timestamp    ,
+            project_id           integer  NOT NULL  ,
+            CONSTRAINT state_pkey PRIMARY KEY ( id )
+         );
 
-		CREATE  TABLE IF NOT EXISTS ho_lock (
-			id                   integer  NOT NULL  ,
-			project_id           integer  NOT NULL  ,
-			created_at           timestamp  NOT NULL  ,
-			updated_at           timestamp  NOT NULL  ,
-			CONSTRAINT pk_ho_lock PRIMARY KEY ( id )
-		 );
-	",
+        CREATE  TABLE IF NOT EXISTS ho_lock (
+            id                   integer  NOT NULL  ,
+            project_id           integer  NOT NULL  ,
+            created_at           timestamp  NOT NULL  ,
+            updated_at           timestamp  NOT NULL  ,
+            CONSTRAINT pk_ho_lock PRIMARY KEY ( id )
+         );
+    ",
     ) {
         Ok(_) => {
             println!("Database migrated successfully!");
@@ -67,9 +67,9 @@ pub fn changes() {
 
     match client.expect("failed to migrate").batch_execute(
         "
-		ALTER TABLE ho_lock ADD CONSTRAINT fk_ho_lock_ho_project FOREIGN KEY ( project_id ) REFERENCES ho_project( id ) ON DELETE CASCADE ON UPDATE CASCADE;
-		ALTER TABLE ho_state ADD CONSTRAINT fk_ho_state_ho_project FOREIGN KEY ( project_id ) REFERENCES ho_project( id ) ON DELETE CASCADE ON UPDATE CASCADE;
-	",
+        ALTER TABLE ho_lock ADD CONSTRAINT fk_ho_lock_ho_project FOREIGN KEY ( project_id ) REFERENCES ho_project( id ) ON DELETE CASCADE ON UPDATE CASCADE;
+        ALTER TABLE ho_state ADD CONSTRAINT fk_ho_state_ho_project FOREIGN KEY ( project_id ) REFERENCES ho_project( id ) ON DELETE CASCADE ON UPDATE CASCADE;
+    ",
     ) {
         Ok(_) => {}
         Err(_) => {}
