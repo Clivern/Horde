8ee0d4de213fd5990a73fc33db0606a51e3d9ffa
Clivern <hello@clivern.com>
2021-07-07 00:34:25 +0200

    update

--- src/db/db.rs
+++ src/db/db.rs
@@ -16,31 +16,31 @@ pub fn tables() {
     match client.expect("failed to migrate").batch_execute(
         "
         CREATE  TABLE IF NOT EXISTS ho_project (
-            id                   integer  NOT NULL  ,
-            name                 varchar(100)  NOT NULL  ,
-            vers                 varchar(100)  NOT NULL  ,
-            created_at           timestamp  NOT NULL  ,
-            updated_at           timestamp  NOT NULL  ,
-            username             varchar(100)  NOT NULL  ,
-            secret               varchar(100)  NOT NULL  ,
+            id                   integer  NOT NULL,
+            name                 varchar(100)  NOT NULL,
+            vers                 varchar(100)  NOT NULL,
+            created_at           timestamp  NOT NULL,
+            updated_at           timestamp  NOT NULL,
+            username             varchar(100)  NOT NULL,
+            secret               varchar(100)  NOT NULL,
             CONSTRAINT pk_ho_project PRIMARY KEY ( id )
          );
 
         CREATE  TABLE IF NOT EXISTS ho_state (
-            id                   integer  NOT NULL  ,
-            name                 varchar(100)    ,
-            val               text  NOT NULL  ,
-            created_at           timestamp    ,
-            updated_at           timestamp    ,
-            project_id           integer  NOT NULL  ,
+            id                   integer  NOT NULL,
+            name                 varchar(100),
+            val                  text  NOT NULL,
+            created_at           timestamp,
+            updated_at           timestamp,
+            project_id           integer  NOT NULL,
             CONSTRAINT state_pkey PRIMARY KEY ( id )
          );
 
         CREATE  TABLE IF NOT EXISTS ho_lock (
-            id                   integer  NOT NULL  ,
-            project_id           integer  NOT NULL  ,
-            created_at           timestamp  NOT NULL  ,
-            updated_at           timestamp  NOT NULL  ,
+            id                   integer  NOT NULL,
+            project_id           integer  NOT NULL,
+            created_at           timestamp  NOT NULL,
+            updated_at           timestamp  NOT NULL,
             CONSTRAINT pk_ho_lock PRIMARY KEY ( id )
          );
     ",
